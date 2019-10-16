f9f783c9fe7d4735eb2aa6bed3052761c93cf254
Nicolas Saulnier <nicks6853@gmail.com>
2019-10-15 21:28:49 -0300

    Changed up the color scheme for i3 borders and added the wallpaper to the repository and the script

--- scripts/i3.sh
+++ scripts/i3.sh
@@ -29,6 +29,7 @@ make && sudo make install
 cd "$(dirname)"
 mkdir -p $HOME/.config/i3 # Ensure directory exists
 cp ../files/i3/config $HOME/.config/i3/config
+cp ../files/i3/wallpaper.jpg $HOME/Pictures/wallpaper.jpg
 
 echo "Installation complete!"
 
