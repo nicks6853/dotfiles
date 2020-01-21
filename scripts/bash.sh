fef7864596b08c6ca303addf2022bc9387f39ff2
Nicolas Saulnier <nicks6853@gmail.com>
2020-01-21 16:48:27 -0400

    Update bashrc

--- scripts/bash.sh
+++ scripts/bash.sh
@@ -2,9 +2,7 @@
 
 set -e
 
-sudo apt-get install -y feh
-
-cat ../files/bash/.bashrc >> $HOME/.bashrc
+cat ../files/bash/.bashrc > $HOME/.bashrc
 
 : > $HOME/.profile
 cp ../files/bash/.profile $HOME/.profile
