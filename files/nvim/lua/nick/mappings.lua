a2f758a5dcd83dff23882f4b75c416fb80c78ec3
nicks6853 <nicks6853@gmail.com>
2025-10-28 22:06:40 -0300

    add mappings for copying text to clipboard and moving between files in the quickfix list

--- files/nvim/lua/nick/mappings.lua
+++ files/nvim/lua/nick/mappings.lua
@@ -13,7 +13,15 @@ vim.keymap.set('n', '<C-l>', ':tabnext<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<C-h>', ':tabprevious<CR>', { noremap = true, silent = true })
 
 -- Add mapping to show LSP errors in a floating window
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { noremap = true })
+
+-- Add mapping to copy to clipboard when in visual mode (block and line included)
+vim.keymap.set('v', '<leader>c', '"+y', { noremap = true })
+
+-- Add mapping to switch between files in the quickfix list
+vim.keymap.set('n', '<leader>n', ':cnext<CR>', { noremap = true })
+vim.keymap.set('n', '<leader>p', ':cprev<CR>', { noremap = true })
+
 
 -- Add mapping for Ctrl+Space to show the autocomplete box
 vim.keymap.set('i', '<c-space>', function()
